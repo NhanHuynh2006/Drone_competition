84807603a42c2ec949a2c43170e5e10fc33ce230
NhanHuynh2006 <huynhlethanhnhan2006@gmail.com>
2026-03-06 13:33:15 +0700

    update

---
+++ build/CMakeFiles/drone_competition.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nolan/Documents/drone_c/src/ball_dropper.c" "CMakeFiles/drone_competition.dir/src/ball_dropper.c.o" "gcc" "CMakeFiles/drone_competition.dir/src/ball_dropper.c.o.d"
+  "/home/nolan/Documents/drone_c/src/dwm1001.c" "CMakeFiles/drone_competition.dir/src/dwm1001.c.o" "gcc" "CMakeFiles/drone_competition.dir/src/dwm1001.c.o.d"
+  "/home/nolan/Documents/drone_c/src/flight_controller.c" "CMakeFiles/drone_competition.dir/src/flight_controller.c.o" "gcc" "CMakeFiles/drone_competition.dir/src/flight_controller.c.o.d"
+  "/home/nolan/Documents/drone_c/src/kalman2d.c" "CMakeFiles/drone_competition.dir/src/kalman2d.c.o" "gcc" "CMakeFiles/drone_competition.dir/src/kalman2d.c.o.d"
+  "/home/nolan/Documents/drone_c/src/mission.c" "CMakeFiles/drone_competition.dir/src/mission.c.o" "gcc" "CMakeFiles/drone_competition.dir/src/mission.c.o.d"
+  "/home/nolan/Documents/drone_c/src/pid.c" "CMakeFiles/drone_competition.dir/src/pid.c.o" "gcc" "CMakeFiles/drone_competition.dir/src/pid.c.o.d"
+  "/home/nolan/Documents/drone_c/src/visual_servo.c" "CMakeFiles/drone_competition.dir/src/visual_servo.c.o" "gcc" "CMakeFiles/drone_competition.dir/src/visual_servo.c.o.d"
+  "/home/nolan/Documents/drone_c/src/detector.cpp" "CMakeFiles/drone_competition.dir/src/detector.cpp.o" "gcc" "CMakeFiles/drone_competition.dir/src/detector.cpp.o.d"
+  "/home/nolan/Documents/drone_c/src/hsv_detector.cpp" "CMakeFiles/drone_competition.dir/src/hsv_detector.cpp.o" "gcc" "CMakeFiles/drone_competition.dir/src/hsv_detector.cpp.o.d"
+  "/home/nolan/Documents/drone_c/src/main.cpp" "CMakeFiles/drone_competition.dir/src/main.cpp.o" "gcc" "CMakeFiles/drone_competition.dir/src/main.cpp.o.d"
+  "/home/nolan/Documents/drone_c/src/sensor_fusion.cpp" "CMakeFiles/drone_competition.dir/src/sensor_fusion.cpp.o" "gcc" "CMakeFiles/drone_competition.dir/src/sensor_fusion.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/drone_competition.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/drone_competition.dir/src/ball_dropper.c.o"
+  "CMakeFiles/drone_competition.dir/src/ball_dropper.c.o.d"
+  "CMakeFiles/drone_competition.dir/src/detector.cpp.o"
+  "CMakeFiles/drone_competition.dir/src/detector.cpp.o.d"
+  "CMakeFiles/drone_competition.dir/src/dwm1001.c.o"
+  "CMakeFiles/drone_competition.dir/src/dwm1001.c.o.d"
+  "CMakeFiles/drone_competition.dir/src/flight_controller.c.o"
+  "CMakeFiles/drone_competition.dir/src/flight_controller.c.o.d"
+  "CMakeFiles/drone_competition.dir/src/hsv_detector.cpp.o"
+  "CMakeFiles/drone_competition.dir/src/hsv_detector.cpp.o.d"
+  "CMakeFiles/drone_competition.dir/src/kalman2d.c.o"
+  "CMakeFiles/drone_competition.dir/src/kalman2d.c.o.d"
+  "CMakeFiles/drone_competition.dir/src/main.cpp.o"
+  "CMakeFiles/drone_competition.dir/src/main.cpp.o.d"
+  "CMakeFiles/drone_competition.dir/src/mission.c.o"
+  "CMakeFiles/drone_competition.dir/src/mission.c.o.d"
+  "CMakeFiles/drone_competition.dir/src/pid.c.o"
+  "CMakeFiles/drone_competition.dir/src/pid.c.o.d"
+  "CMakeFiles/drone_competition.dir/src/sensor_fusion.cpp.o"
+  "CMakeFiles/drone_competition.dir/src/sensor_fusion.cpp.o.d"
+  "CMakeFiles/drone_competition.dir/src/visual_servo.c.o"
+  "CMakeFiles/drone_competition.dir/src/visual_servo.c.o.d"
+  "drone_competition"
+  "drone_competition.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/drone_competition.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
